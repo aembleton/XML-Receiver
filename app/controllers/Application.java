d3aef6da83386236b7744f1334651f6514e54024
Arthur Embleton <arthur@blerg.net>
2011-02-15 17:37:47 +0000

    Receives XML at / and displays this in the console and returns as part of a JSON response

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,16 +1,60 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
 
-import java.util.*;
-
-import models.*;
+import play.Logger;
+import play.mvc.Controller;
 
 public class Application extends Controller {
 
-    public static void index() {
-        render();
-    }
+	public static void index() {
+		render();
+	}
 
+	public static void receiveXml() {
+		String streamAsString = "";
+
+		try {
+			streamAsString = convertStreamToString(request.body);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		Logger.error(streamAsString);
+
+		renderJSON(streamAsString);
+	}
+
+	public static String convertStreamToString(InputStream is) throws IOException {
+		/*
+		 * To convert the InputStream to String we use the Reader.read(char[]
+		 * buffer) method. We iterate until the Reader return -1 which means
+		 * there's no more data to read. We use the StringWriter class to
+		 * produce the string.
+		 */
+		if (is != null) {
+			Writer writer = new StringWriter();
+
+			char[] buffer = new char[1024];
+			try {
+				Reader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+				int n;
+				while ((n = reader.read(buffer)) != -1) {
+					writer.write(buffer, 0, n);
+				}
+			} finally {
+				is.close();
+			}
+			return writer.toString();
+		} else {
+			return "";
+		}
+	}
 }
